5bdf54ecf6c0b5ff14f4b7668cb75ba6a0d8256c
fila man <flimonataklti@gmail.com>
2017-06-02 21:58:33 +0530

    I added index.html and redirect to other sites using href

--- app/views.py
+++ app/views.py
@@ -3,4 +3,7 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    context = {}
+    return render(request, 'app/index.html', context)
+
