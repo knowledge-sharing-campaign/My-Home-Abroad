8156ff254b44e7399ab1f39ffbc16481305adb3a
fila man <flimonataklti@gmail.com>
2017-06-03 00:14:25 +0530

    i have included taskbar card from trello

--- app/urls.py
+++ app/urls.py
@@ -5,5 +5,10 @@ from . import views
 app_name = 'app'
 urlpatterns = [
     url(r'^$', views.index, name='index'), 
-    url(r'^test/$', views.test, name='test'), 
+    url(r'^login/$', views.login, name='login'),
+    url(r'^logout/$', views.logout, name='logout'),
+    url(r'^policies/$', views.policies, name='policies'),
+    url(r'^refer/$', views.refer, name='refer'),
+    url(r'^schedule/$', views.schedule, name='schedule'),
+    url(r'^volunteer/$', views.volunteer, name='volunteer'), 
 ]
--- app/views.py
+++ app/views.py
@@ -6,6 +6,21 @@ from django.shortcuts import render
 def index(request):
     context = {}
     return render(request, 'app/index.html', context)
-def test(request):
+def login(request):
 	context = {}
-	return render(request, 'app/test.html', context)
+	return render(request, 'app/login.html', context)
+def logout(request):
+	context = {}
+	return render(request, 'app/logout.html', context)
+def policies(request):
+	context = {}
+	return render(request, 'app/policies.html', context)
+def refer(request):
+	context = {}
+	return render(request, 'app/refer.html', context)
+def schedule(request):
+	context = {}
+	return render(request, 'app/schedule.html', context)
+def volunteer(request):
+	context = {}
+	return render(request, 'app/volunteer.html', context)
